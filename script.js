5f6022bdbeccdb918edb316fdebe5d55fe2a259a
Gregor <43413852+Albadylic@users.noreply.github.com>
2022-11-23 17:42:37 +0000

    Create code read

---
+++ script.js
@@ -0,0 +1,22 @@
+const input = document.querySelector("#input-box");
+const submitButton = document.querySelector("button");
+const output = document.querySelector("output");
+
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (input.value === "") {
+    output.textContent = "Please enter an input";
+  } else {
+    output.value = checkInput(input.value);
+  }
+});
+
+function checkInput(string) {
+  const vowels = "aeiou";
+
+  let count = string
+    .split("")
+    .filter((letter) => vowels.includes(letter)).length;
+
+  return count;
+}
